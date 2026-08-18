113191aa7acafbdab8e37af737e1413923fc9308
Patrick <teamgeist2007@gmail.com>
2026-08-18 16:22:26 +0200

    changed js + css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const ImageCount = 12;
 
 function renderImageCount(){
     for(let i = 1; i <= ImageCount; i++){
-        document.getElementById('photo-gallery').innerHTML += `<img src="./assets/img/Bild${i}.jpeg" alt="Bild ${i}">`;
+        document.getElementById('photo-gallery').innerHTML += `<img class="gallery-image" src="./assets/img/Bild${i}.jpeg" alt="Bild ${i}">`;
     }
 }
 
