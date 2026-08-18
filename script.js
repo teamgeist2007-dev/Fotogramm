3515779e32ea81614b4f7c66d796105a2f387a5a
Patrick <teamgeist2007@gmail.com>
2026-08-18 19:15:34 +0200

    changed js

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const ImageCount = 12;
 
 function renderImageCount(){
     for(let i = 1; i <= ImageCount; i++){
-        document.getElementById('photo-gallery').innerHTML += `<img class="gallery-image" src="./assets/img/Bild${i}.jpeg" alt="Bild ${i}">`;
+        document.getElementById('photo-gallery').innerHTML += `<img class="gallery-image" src="./assets/img/Bild${i}.jpeg" alt="Bild ${i}" onclick="openImage(${i})">`;
     }
+}
+
+function openImage(i){
+    console.log(i);
 }
 
 renderImageCount();
