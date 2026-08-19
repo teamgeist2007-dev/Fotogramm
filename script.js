6db9b154fd19e17f858ce4df0610900ac5ad3404
Patrick <teamgeist2007@gmail.com>
2026-08-19 15:23:55 +0200

    changed alles

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const ImageCount = 12;
 
+const ImageNames = [
+    'Unterkunft in Moniga del Garda',
+    'Platz in der Altstadt',
+    'Balkon mit Blumen',
+    'Altstadt mit Palmen',
+    'Hotel Castello in Sirmione',
+    'Zugbrücke der Scaligerburg in Sirmione',
+    'Eingang der Scaligerburg in Sirmione',
+    'Zitronenbaum am Gardasee',
+    'Steinbrunnen am Gardasee',
+    'Villa am Gardasee',
+    'Blick über den Gardasee',
+    'Straße in der Scaligerburg'
+];
+
 function renderImageCount(){
     for(let i = 1; i <= ImageCount; i++){
         document.getElementById('photo-gallery').innerHTML += `<img class="gallery-image" src="./assets/img/Bild${i}.jpeg" alt="Bild ${i}" onclick="openImage(${i})">`;
@@ -9,10 +24,11 @@ function renderImageCount(){
 function openImage(i){
     const imageDialog = document.getElementById('image-dialog');
     const dialogImage = document.getElementById('dialog-image');
+    const imageName = ImageNames[i - 1];
 
     dialogImage.src = `./assets/img/Bild${i}.jpeg`;
-    dialogImage.alt = `Bild ${i}`;
-    document.getElementById('dialog-title').innerHTML = `Bild ${i}`;
+    dialogImage.alt = imageName;
+    document.getElementById('dialog-title').innerHTML = imageName;
     imageDialog.showModal();
     document.getElementById('dialog-title').focus();
 }
